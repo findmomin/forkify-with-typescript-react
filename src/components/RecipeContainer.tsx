8757c83b4aa70e7b8af3fca8c3561dd5092c8806
Abdul Momin <abdulmomin114477@gmail.com>
2021-01-25 08:50:39 +0600

    all

--- src/components/RecipeContainer.tsx
+++ src/components/RecipeContainer.tsx
@@ -7,28 +7,22 @@ export default function Recipe() {
   return (
     <div className={styles.RecipeContainer}>
       <Switch>
-        {/* Message */}
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <Message>
-              <svg>
-                <use href={`${icons}#icon-smile`}></use>
-              </svg>
-              <p>Start by searching for a recipe or an ingredient. Have fun!</p>
-            </Message>
-          )}
-        />
-
         {/* Recipe */}
         <Route
           exact
-          path="/:id"
+          path="/:query/:id"
           render={() => {
             return <h1>Recipe</h1>;
           }}
         />
+
+        {/* Message */}
+        <Message>
+          <svg>
+            <use href={`${icons}#icon-smile`}></use>
+          </svg>
+          <p>Start by searching for a recipe or an ingredient. Have fun!</p>
+        </Message>
       </Switch>
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { Switch, Route } from 'react-router-dom';
 import styles from '../styles/Sidebar.module.css';
 import icons from '../Images/icons.svg';
 import Message from './styled/Message';
@@ -5,15 +6,24 @@ import Message from './styled/Message';
 export default function SearchResults() {
   return (
     <div className={styles.Sidebar}>
-      {/* Message */}
-      <Message>
-        <svg>
-          <use href={`${icons}#icon-smile`}></use>
-        </svg>
-        <p>Search for a recipe to get search results!</p>
-      </Message>
+      <Switch>
+        {/* Message */}
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Message>
+              <svg>
+                <use href={`${icons}#icon-smile`}></use>
+              </svg>
+              <p>Search for a recipe to get search results!</p>
+            </Message>
+          )}
+        />
+      </Switch>
 
       {/* Search Results */}
+      <Route path="/:query" render={() => <h1>Results here</h1>} />
 
       {/* Footer text */}
       <p className={styles.Copyright}>&copy; by Abdul Momin</p>
