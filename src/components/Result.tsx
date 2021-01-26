acc13424da7e0007639d243940f31f5f8e53f999
Abdul Momin <abdulmomin114477@gmail.com>
2021-01-26 10:15:09 +0600

    all

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,5 +1,34 @@
-const Result = () => {
-  return <div>result</div>;
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import icons from '../Images/icons.svg';
+import styles from '../styles/Result.module.css';
+import * as Types from '../Types';
+
+interface Props {
+  result: Types.Result;
+}
+
+const Result: React.FC<Props> = ({
+  result: { id, title, publisher, image_url },
+}) => {
+  const { query } = useParams<{ query: string }>();
+
+  return (
+    <Link to={`/${query}/${id}`} className={styles.Link}>
+      <figure className={styles.Figure}>
+        <img src={image_url} alt={title} />
+      </figure>
+      <div className={styles.Data}>
+        <h4 className={styles.Title}>{title}</h4>
+        <p className={styles.Publisher}>{publisher}</p>
+        <div className={styles.UserGenerated}>
+          <svg>
+            <use href={`${icons}#icon-user`}></use>
+          </svg>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default Result;
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
-import { ResultsContext } from '../contexts/Results.context';
 import { useRouteMatch } from 'react-router-dom';
+import { ResultsContext } from '../contexts/Results.context';
+import styles from '../styles/Results.module.css';
 import Result from './Result';
 
 const Results: React.FC = () => {
@@ -12,16 +13,21 @@ const Results: React.FC = () => {
     params: { newQuery },
   } = useRouteMatch<{ newQuery: string }>('/:newQuery')!;
 
+  // Setting the query in the context if the query is new
   useEffect(() => {
     if (query === newQuery) return;
 
     setQuery!(newQuery);
   });
 
-  const markup = results?.map(result => <Result key={result.id} />);
+  const markup = results?.map(result => (
+    <Result key={result.id} result={result} />
+  ));
 
   return (
-    <div className={''}>{results?.length ? markup : <h1>Rendering</h1>}</div>
+    <div className={styles.Results}>
+      {results?.length ? markup : <h1>Rendering</h1>}
+    </div>
   );
 };
 
